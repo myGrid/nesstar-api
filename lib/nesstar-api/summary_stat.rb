829025c06f6ff4ae9ebc85abcabfb11d593129c8
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2011-03-11 15:46:01 +0000

    initial commit

---
+++ lib/nesstar-api/summary_stat.rb
@@ -0,0 +1,11 @@
+module Nesstar
+  
+  #Stats at the variable level, eg how many valid or invalid rows
+  class SummaryStat
+  
+    attr_reader :type, :value
+    attr_writer :type, :value
+  
+  end
+  
+end
---
+++ lib/nesstar-api/version.rb
@@ -0,0 +1,5 @@
+module Nesstar
+  module Api
+    VERSION = "0.0.2"
+  end
+end
